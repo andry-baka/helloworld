12487f0fd2718356dc953bca99c674f4b058eddc
andryb <andry.baka@mitrais.com>
2016-02-11 16:48:37 +0800

    First commit

---
+++ app/app-component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {RandomColorDirective} from "./random-color-directive";
+
+@Component({
+    selector: "my-app",
+    template: `<h3 randomColor>{{title}}</h3>`,
+    directives: [RandomColorDirective]
+})
+export class AppComponent {
+    private title:string = "Angular 2.0 is in the house..!!!";
+}
